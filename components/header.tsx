c0061c08d9e2405a6516251faa35828c7c7fc4fc
Yeonhee Hayden Kim <devyeony@gmail.com>
2025-04-27 03:07:16 +0900

    style: adjust header menu spacing and update home icon

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
+import { Home } from "lucide-react";
 
 interface HeaderProps {
   className?: string;
@@ -32,21 +33,30 @@ export const Header: React.FC<HeaderProps> = ({ className = "" }) => {
   return (
     <header ref={ref} className={className}>
       <div
-        className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
+        className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b  ${
           isIntersecting
             ? "bg-zinc-900/0 border-transparent"
             : "bg-zinc-900/500  border-zinc-800 "
         }`}
       >
         <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
-          <div className="flex justify-between gap-8">
+          <div className="flex justify-between gap-4">
             {navigation.map((item) => (
               <Link
                 key={item.href}
                 href={item.href}
-                className="relative duration-200 text-zinc-300 hover:text-zinc-100 after:content-[''] after:absolute after:left-[-4px] after:right-[-4px] after:bottom-[-6px] after:h-1 after:w-0 after:bg-green-300 after:rounded-full after:transition-all after:duration-300 hover:after:w-[calc(100%+8px)]"
+                className="group relative duration-200 text-zinc-300 hover:text-purple-300"
               >
-                {item.name}
+                {item.name === "Home" ? (
+                  <Home
+                    size={25}
+                    className="duration-200 text-inherit"
+                  />
+                ) : (
+                  <span className="duration-200 text-inherit">
+                    {item.name}
+                  </span>
+                )}
               </Link>
             ))}
           </div>
