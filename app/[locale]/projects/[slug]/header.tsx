2668a1270913d2c5c48e326334cef22534bf883f
Yeonhee Hayden Kim <devyeony@gmail.com>
2025-06-09 20:34:23 +0900

    style: update back button and title styling

--- app/[locale]/projects/[slug]/header.tsx
+++ app/[locale]/projects/[slug]/header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useLocale } from "next-intl";
-import { Link as I18nLink } from '@/i18n/routing';
+import { Link as I18nLink } from "@/i18n/routing";
 import React, { useEffect, useRef, useState } from "react";
 import { formatDateRange } from "@/utils/date-util";
 import { ArrowLeft } from "lucide-react";
@@ -70,7 +70,7 @@ export const Header: React.FC<Props> = ({ project }) => {
     : {
         background: "linear-gradient(to top left, black, #333, black)",
       };
-  
+
   const locale = useLocale();
 
   return (
@@ -79,10 +79,10 @@ export const Header: React.FC<Props> = ({ project }) => {
       className="relative isolate overflow-hidden"
       style={backgroundStyle}
     >
-      <div className="absolute top-6 left-6 z-10 rounded-full bg-green-300/80">
+      <div className="absolute w-10 h-10 sm:w-14 sm:h-14 top-6 left-6 z-10 rounded-full bg-black/80">
         <I18nLink
           href="/projects"
-          className={`flex items-center justify-center rounded-full border-2 border-white p-2 duration-200 hover:scale-110 ${
+          className={`flex items-center justify-center w-full h-full rounded-full border-2 border-white p-2 duration-200 hover:scale-110 ${
             isIntersecting
               ? "text-zinc-400 hover:text-zinc-100"
               : "text-zinc-600 hover:text-zinc-900"
@@ -94,12 +94,12 @@ export const Header: React.FC<Props> = ({ project }) => {
       <div className="container mx-auto relative isolate overflow-hidden py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <h1 className="text-3xl font-bold text-white sm:text-6xl font-display text-shadow-md">
-              <span className="bg-black/70 border-8 px-4 py-2">
+            <h1 className="mx-auto sm:max-w-xl text-2xl font-bold text-white sm:text-6xl font-display text-shadow-md leading-snug break-words">
+              <span className="inline-block bg-black/70 border-8 px-4 py-2">
                 {project.title}
               </span>
             </h1>
-            <div className="text-xl font-bold mt-10">
+            <div className="text-xl font-bold mt-6">
               <span className="bg-white/70 px-1">
                 {formatDateRange(project.startDate, project.endDate, locale)}
               </span>
@@ -116,21 +116,21 @@ export const Header: React.FC<Props> = ({ project }) => {
           </div>
 
           {links.length > 0 && (
-  <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-    <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
-      {links.map((link) => (
-        <div
-          key={link.label}
-          className="font-mono bg-black text-white border-2 px-2 hover:bg-purple-200 hover:text-black hover:border-black rounded-sm"
-        >
-          <Link target="_blank" href={link.href}>
-            {link.label} <span aria-hidden="true">&rarr;</span>
-          </Link>
-        </div>
-      ))}
-    </div>
-  </div>
-)}
+            <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+              <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
+                {links.map((link) => (
+                  <div
+                    key={link.label}
+                    className="font-mono bg-black text-white border-2 px-2 hover:bg-purple-200 hover:text-black hover:border-black rounded-sm"
+                  >
+                    <Link target="_blank" href={link.href}>
+                      {link.label} <span aria-hidden="true">&rarr;</span>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
