d13654ed13300c54b84b9e54e19adb673d94785b
Yeonhee Hayden Kim <devyeony@gmail.com>
2025-06-23 02:36:41 +0900

    refactor: simplify YoutubeEmbed component with cleaner styles

--- components/youtube-embed.tsx
+++ components/youtube-embed.tsx
@@ -1,24 +1,28 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 
 export interface YoutubeEmbedProps {
-  videoId: string
+  videoId: string;
+  title?: string;
 }
 
-const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ videoId }) => {
+const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({
+  videoId,
+  title = "YouTube video",
+}) => {
   return (
     <div className="relative w-full pb-[56.25%] h-0">
       <iframe
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-full h-full border-0"
         src={`https://www.youtube.com/embed/${videoId}`}
-        title="YouTube video"
-        frameBorder="0"
+        title={title}
+        loading="lazy"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
     </div>
-  )
-}
+  );
+};
 
-export default YoutubeEmbed
+export default YoutubeEmbed;
